01d20808e77e44c31b68cec01ee96581f6d74aec
Vandana Kumari <vandanasinghbhore04@gmail.com>
2026-07-06 14:34:58 +0530

    Update backend URL for production

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,8 +1,4 @@
 let IS_PROD = true;
-const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
-
-    "http://localhost:8000"
-
+const server = "https://meetflow-jik3.onrender.com";
 
 export default server;
