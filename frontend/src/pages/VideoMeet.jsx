7d60aa9e8850c4f56012a814b48600bb36f596f5
Vandana Kumari <vandanasinghbhore04@gmail.com>
2026-07-07 15:37:35 +0530

    Prevent unnecessary video reload causing blink

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -990,12 +990,12 @@ useEffect(() => {
                       {videos.map((video) => (
     <div key={video.socketId}>
         <video data-socket={video.socketId}
-            ref={ref => {
-                if (ref && video.stream) {
-                    ref.srcObject = video.stream;
-                    ref.play().catch(e => console.log("Remote play error:", e));
-                }
-            }}
+          ref={ref => {
+    if (ref && video.stream && ref.srcObject !== video.stream) {
+        ref.srcObject = video.stream;
+        ref.play().catch(e => console.log("Remote play error:", e));
+    }
+}} 
             autoPlay
             playsInline
             muted={false}>
